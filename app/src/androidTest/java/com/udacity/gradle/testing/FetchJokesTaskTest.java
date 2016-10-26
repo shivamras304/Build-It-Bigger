727ebb606ff33c5261577ccd7aeb3660454b2010
Shivam Rastogi <shivam@MacBook-Air.local>
2016-10-27 05:07:39 +0530

    Step 4 completed, Android Test added

--- app/src/androidTest/java/com/udacity/gradle/testing/FetchJokesTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/testing/FetchJokesTaskTest.java
@@ -2,6 +2,7 @@ package com.udacity.gradle.testing;
 
 
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import com.udacity.gradle.chutkula.FetchJokeTask;
 
@@ -11,21 +12,30 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.ExecutionException;
+
 @RunWith(AndroidJUnit4.class)
 public class FetchJokesTaskTest{
+
+    final String LOG_TAG = getClass().getName();
 
     String response = null;
 
     @Before
     public void cleaning() {
-        FetchJokeTask.response = null;
+        response = null;
     }
 
     @Test
-    public void test() {
-        FetchJokeTask fetchJokeTask = new FetchJokeTask();
-        fetchJokeTask.execute();
+    public void test() throws ExecutionException, InterruptedException {
 
+        try {
+            FetchJokeTask fetchJokeTask = new FetchJokeTask();
+            fetchJokeTask.execute().get();
+        }
+        catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
         response = FetchJokeTask.response;
 
         Assert.assertNotNull(response);
